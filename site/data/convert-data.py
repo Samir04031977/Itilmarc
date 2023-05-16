914cc7c1defe8d501b9223bc4414e3626370e7f0
Marc Baloup <marc.baloup@laposte.net>
2023-05-16 09:52:26 +0200

    Update data

---
+++ site/data/convert-data.py
@@ -0,0 +1,39 @@
+import csv
+import json
+
+questions = {}
+
+with open('questions.csv', encoding="utf-8") as questionsFile:
+    questionsData = csv.reader(questionsFile, delimiter=";")
+    next(questionsData, None) # skip header
+    for questionLine in questionsData:
+        qId = questionLine[0]
+        qTxt = questionLine[1]
+        ansId = questionLine[2]
+        ansTxt = questionLine[3]
+        if not qId in questions:
+            questions[qId] = {
+                "question": qTxt,
+                "answers": {}
+            }
+
+        if ansId.isdigit():
+            # ansId is a number, so its actually part of the question
+            questions[qId]["question"] += f"\n{ansId}. {ansTxt}"
+        else:
+            # actually an answer
+            questions[qId]["answers"][ansId] = ansTxt
+
+with open('good-answers.csv', encoding="utf-8") as answerFile:
+    answerData = csv.reader(answerFile, delimiter=";")
+    next(answerData, None) # skip header
+    for answerLine in answerData:
+        qId = answerLine[0]
+        goodAns = answerLine[1]
+        questions[qId]["good"] = goodAns
+
+jsonData = json.dumps(questions, indent="  ", ensure_ascii=False)
+
+with open('../js/data.json', 'w', encoding='utf-8') as jsonFile:
+    jsonFile.write(jsonData)
+
